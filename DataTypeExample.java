63e675c2ab2897872604843eef8d6dd0549d8201
pavitrapatil2017 <pavikp@gmail.com>
2025-02-10 15:20:39 +0530

    fileadded

---
+++ DataTypeExample.java
@@ -0,0 +1,16 @@
+class DataTypeExample
+{
+public static void main(String[] a)//method or function 
+{
+//local variable
+float marks=34.34f;
+System.out.println(marks);
+char grade='A';
+short age = 23;
+boolean status=false;
+System.out.print(grade+" ");
+System.out.print(age+" ");
+System.out.print(status);
+
+}
+}
